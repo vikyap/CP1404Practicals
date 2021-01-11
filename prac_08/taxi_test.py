3934812e3d0aaefd897cfac0dd5a6b122509338d
V\vicya <vikyap11@gmail.com>
2021-01-11 23:14:57 +0800

    edited code to exclude price_per_km value

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,7 +2,7 @@ from prac_08.taxi import Taxi
 
 
 def main():
-    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi = Taxi("Prius 1", 100)
     new_taxi.drive(40)
     print("Taxi details:", new_taxi)
     print("Payable fare is: $", new_taxi.get_fare())
