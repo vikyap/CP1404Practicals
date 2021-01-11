704036e8da8f187479153c634f6fd584b39456c4
V\vicya <vikyap11@gmail.com>
2021-01-11 23:11:30 +0800

    completed code to test taxi class

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,15 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi.drive(40)
+    print("Taxi details:", new_taxi)
+    print("Payable fare is: $", new_taxi.get_fare())
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print("Taxi details:", new_taxi)
+    print("Payable fare is: $", new_taxi.get_fare())
+
+
+main()
