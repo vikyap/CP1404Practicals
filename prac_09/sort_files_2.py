7dc3a7cc17589616cc6a51af5b25fa8358e7aca4
V\vicya <vikyap11@gmail.com>
2021-01-20 15:02:59 +0800

    complete sort_files_2

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -11,4 +11,13 @@ def main():
         if os.path.isdir(file):
             pass
         extension = file.split(".")[-1]
+        folder = input("What folder does {} go into?".format(extension))
+        category[extension] = folder
 
+        if os.path.exists(source + '/' + extension):
+            shutil.move(source + '/' + file, source + '/' + extension + '/' + file)
+        else:
+            os.mkdir(source + "/" + extension)
+            shutil.move(source + '/' + file, source + '/' + extension + "/" + file)
+
+main()
